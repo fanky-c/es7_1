07a53904a31ab307199e0a6cdebd563e63eb7f88
zhaochao <396850524@qq.com>
2016-10-17 14:09:08 +0800

    no message

---
+++ src/js/foo.js
@@ -0,0 +1,15 @@
+class Person{
+	 constructor(_name = 'chao', _age = 12){
+         this.name = _name;
+         this.age = _age;
+	 }
+	 getName(){
+	 	  return this.name;
+	 }
+	 getAge(){
+	 	  return this.age; 
+	 }
+}
+
+
+export default Person;
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import p from './foo';
+
+let p1 = new p();
+alert('my name is: '+p1.getName())
